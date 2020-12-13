c3964426775f1f5bbd2fb3c976cc6d12703df2e8
kemiranda <kjmd2s@gmail.com>
2020-12-12 23:54:36 -0500

    Mejorando código de recetas

--- src/RecipesMaker.kt
+++ src/RecipesMaker.kt
@@ -1,3 +1,5 @@
+import java.awt.List
+
 fun main(args: Array<String>){
     println(":: Bienvenido a Recipe Maker ::")
     val bienvenida = """Selecciona la opción deseada
@@ -30,8 +32,53 @@ fun main(args: Array<String>){
 
 fun seeTheRecets() {
     println("\nAquí estarán guardadas tus recetas...\n")
+    /*for((k,v) in recetas){
+        println("$k: $v")
+    }*/
+
 }
 
-fun doRecets() {
-    println("\nAquí podrás realizar tus recetas nuevas\n")
+fun doRecets()//: MutableMap<String?, String>
+ {
+    //println("\nAquí podrás realizar tus recetas nuevas\n")
+    val menuIngredientes = """
+        Elija los ingredientes de su receta, separados por coma, ejemplo:
+        1,2,5
+        
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+        6. Cereal
+        7. Huevos
+        8. Aceite
+    """.trimIndent()
+    println("\n"+menuIngredientes+"\n")
+    val ingredientes = mapOf<Int, String> ( 1 to "Agua",
+                                        2 to "Leche",
+                                        3 to "Carne",
+                                        4 to "Verduras",
+                                        5 to "Frutas",
+                                        6 to "Cereal",
+                                        7 to "Huevos",
+                                        8 to "Aceite")
+    val entradaIngredientes: String? = readLine()
+    println("\nIngrese un nombre para su receta...")
+    val nombreReceta: String = readLine().toString()
+
+    val splitIngredientes = entradaIngredientes!!.split(",")
+    val misRecetas = mutableMapOf<String, ArrayList<String?>>()
+    val seleccionados = arrayListOf<String?>()
+    for ( elements in splitIngredientes){
+        seleccionados.add(ingredientes[elements.toInt()])
+    }
+    misRecetas.put(nombreReceta, seleccionados)
+    val itr = misRecetas.values
+    for (k in misRecetas.keys){
+        println("$k:")
+        itr.forEach{
+            println(it)
+        }
+    }
 }
